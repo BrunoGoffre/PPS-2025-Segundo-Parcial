f651ee406f9b36a1529799880d9c806befac57b6
Alex Stocki <ladislao.stocki@gointegro.com>
2025-06-21 16:58:08 -0300

    config: firebase - authentication - firestore

--- Restaurante/src/main.ts
+++ Restaurante/src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+// import { appConfig } from './app/app.config';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(HttpClientModule),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    provideAnimationsAsync(),
+  ],
+});
