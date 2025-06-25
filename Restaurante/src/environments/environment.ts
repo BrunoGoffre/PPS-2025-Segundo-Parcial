8726050e2e1f83de6e1fa04b9472f52b44b1edeb
Bruno Goffredo <bruno.goffredo@linbis.com>
2025-06-25 16:02:35 -0300

    enviroment restore

---
+++ Restaurante/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: 'AIzaSyAJA-lRQPUVorGD3lmTlbBCJfyuD5qRSyY',
+      authDomain: 'pps-bonappetit.firebaseapp.com',
+      projectId: 'pps-bonappetit',
+      storageBucket: 'pps-bonappetit.firebasestorage.app',
+      messagingSenderId: '209035081342',
+      appId: '1:209035081342:web:bc432a2d8002edf5ef4a2f'
+    },
+    supabase: {
+      url: 'https://tmqdotautozmpgozytsi.supabase.co',
+      key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcWRvdGF1dG96bXBnb3p5dHNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0MTE3MDgsImV4cCI6MjA2NDk4NzcwOH0.KFTvW999zGjHZim0gsFtdcy-BLcfQdApfvzAkl59LfY',
+      bucket: 'avatars'
+    }
+  };
