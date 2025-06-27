03f86bd001d1937df430fca861d2615ee7a16811
Alex Stocki <ladislao.stocki@gointegro.com>
2025-06-26 22:26:50 -0300

    fix: anon user nombre

--- Restaurante/src/app/register/register.page.ts
+++ Restaurante/src/app/register/register.page.ts
@@ -131,7 +131,7 @@ export class RegisterPage implements OnInit {
         } else {
           const randomDigits = Math.floor(100000 + Math.random() * 900000);
           this.cliente!.mail = `anon_${randomDigits}@anon.com`;
-          this.cliente!.nombre = 'Anónimo';
+          // this.cliente!.nombre = 'Anónimo';
           this.cliente!.estadoAprobacion = 'aprobado';
 
           // Usar el nuevo método de registro con Supabase
